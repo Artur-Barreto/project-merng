b8a52ac240a2c494cf4435f8abccd4a7eb0d45d6
Artur-barreto <artur.barreto7@outlook.com>
2021-05-21 17:26:47 -0300

    projeto iniciado e com a inclusao do mongo e do graphql

---
+++ graphql/typeDefs.js
@@ -0,0 +1,15 @@
+const {gql} = require('apollo-server')
+
+
+module.exports = gql `
+    type Post{
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+    type Query {
+        getPosts:[Post]
+    }
+
+`
